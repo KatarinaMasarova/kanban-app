2c6e1df12964921db0d7cd7325f711822c428a4c
katka <masarova.katar@gmail.com>
2019-05-30 17:15:41 +0200

    zakladne komponenty

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
+import KanbanBoard from './KanbanBoard/KanbanBoard';
 
 class App extends Component {
   render() {
     return (
-      <div> fungujem 
-      </div>
+      <KanbanBoard />
     );
   }
 }
