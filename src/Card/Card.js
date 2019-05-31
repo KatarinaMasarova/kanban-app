923d51c7aa0f2eb54719fc99a44e4e66525f981e
katka <masarova.katar@gmail.com>
2019-05-31 14:47:10 +0200

    css + toggle cards

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -3,14 +3,30 @@ import React, { Component } from 'react';
 import CheckList from '../CheckList/CheckList';
 
 class Card extends Component {
+    state = {
+        showDetails: false
+    };
+
+    toggleCard = () => {
+        this.setState({showDetails: !this.state.showDetails});
+    }
+
     render() {
-        return (
-            <div className="card">
-                <div className="card__title">{this.props.title}</div>
+        console.log(this.state.showDetails);
+        let cardDetails = null;
+        if (this.state.showDetails) {
+            cardDetails = (
                 <div className="card__details">
                     {this.props.description}
                     <CheckList cardId={this.props.id} tasks={this.props.tasks} />
                 </div>
+            );
+        };
+
+        return (
+            <div className="card" onClick={this.toggleCard}>
+                <div className="card__title">{this.props.title}</div>
+                {cardDetails}
             </div>
         );
     }
